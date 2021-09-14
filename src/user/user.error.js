d142dd25b9346b48019119ed9d35129a5d8ff516
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2021-09-15 00:31:57 +0700

    operational distinguished

--- src/user/user.error.js
+++ src/user/user.error.js
@@ -1,9 +1,10 @@
-function UserError(name, httpCode, description) {
+function UserError(name, httpCode, description, isOperational) {
   Error.call(this);
   Error.captureStackTrace(this);
   this.name = name;
   this.description = description;
   this.httpCode = httpCode;
+  this.isOperational = isOperational;
 }
 
 UserError.prototype = Object.create(Error.prototype);
